114cdb1a0f62c82d216b8e292114995d235f3768
studentqa2023 <sarowerhome@gmail.com>
2022-12-29 21:33:56 -0500

    Added NewYear class

---
+++ src/main/java/com/generic/NewYear.java
@@ -0,0 +1,14 @@
+package com.generic;
+
+public class NewYear {
+	
+	public void getYear() {
+		System.out.println("Happy New Year!");
+	}
+	
+	public static void main(String[] args) {
+		NewYear obj = new NewYear();
+		obj.getYear();
+	}
+
+}
